89a7143d835f904645b1f252a310dd78121087c8
Matt Stevens <matt@mattstevens.co.uk>
2022-05-16 12:08:53 +0100

    Base empty s&box game

---
+++ code/Game.cs
@@ -0,0 +1,18 @@
+﻿global using Sandbox;
+global using Sandbox.UI;
+global using Sandbox.UI.Construct;
+global using Sandbox.Component;
+global using Hammer;
+
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.ComponentModel;
+global using System.Threading.Tasks;
+
+namespace TableTennis;
+
+public partial class TableTennisGame : Game
+{
+
+}
