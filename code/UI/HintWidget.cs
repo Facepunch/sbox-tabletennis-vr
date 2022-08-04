5b7aa12c90f8a6c8aa401bf42502792f0b9f6948
Tony <devultj@gmail.com>
2022-08-04 17:57:14 +0100

    Can specify an icon instead of avatar for hint messages

--- code/UI/HintWidget.cs
+++ code/UI/HintWidget.cs
@@ -2,8 +2,16 @@ namespace TableTennis;
 
 internal class HintWidgetEntry : Panel
 {
-	public HintWidgetEntry( string text, float lifetime = 5f )
+	public HintWidgetEntry( string text, float lifetime = 5f, string icon = null )
 	{
+		if ( !string.IsNullOrEmpty( icon ) )
+		{
+			if ( icon.StartsWith( "avatar:" ) )
+				Add.Image( icon, "avatar" );
+			else
+				Add.Label( icon, "icon" );
+		}
+
 		Add.Label( text, "message" );
 		_ = DeleteAsync( lifetime );
 	}
@@ -29,9 +37,9 @@ public partial class HintWidget : WorldPanel
 		Current = this;
 	}
 
-	public void AddMessage( string text, float lifetime = 5f )
+	public void AddMessage( string text, float lifetime = 5f, string icon = null )
 	{
-		Canvas.AddChild( new HintWidgetEntry( text, lifetime ) );
+		Canvas.AddChild( new HintWidgetEntry( text, lifetime, icon ) );
 	}
 
 	public override void Tick()
@@ -46,11 +54,12 @@ public partial class HintWidget : WorldPanel
 	[ConCmd.Client( "tt_hinttest" )]
 	public static void HintTest()
 	{
-		Current.AddMessage( "DevulTj joined the game", 5f );
+		Current.AddMessage( "DevulTj joined the game", 5f, "login" );
 		var a = async () =>
 		{
+
 			await GameTask.DelaySeconds( 4f );
-			Current.AddMessage( "matt left the game", 5f );
+			Current.AddMessage( "matt left the game", 5f, "logout" );
 			
 		};
 		_ = a();
