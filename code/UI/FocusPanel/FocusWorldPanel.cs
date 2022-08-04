ad1c01b66036d8f426692737ae5ca96d8afd5098
Tony <devultj@gmail.com>
2022-08-04 17:29:36 +0100

    Use a statically mounted world panel for scores

--- code/UI/FocusPanel/FocusWorldPanel.cs
+++ code/UI/FocusPanel/FocusWorldPanel.cs
@@ -5,7 +5,7 @@ public partial class FocusWorldPanel : WorldPanel
 	// These panels can define where they'll be relative to the player
 	protected float Forward { get; set; } = 50f;
 	protected float Right { get; set; } = 0;
-	protected float Up { get; set; } = 50f;
+	protected float Up { get; set; } = 10f;
 
 	Transform head => Input.VR.Head;
 
@@ -38,11 +38,11 @@ public partial class FocusWorldPanel : WorldPanel
 	public override void Tick()
 	{
 		var focus = GetFocus();
-		Position = focus.Position;
-		Rotation = focus.Rotation;
+		Position = Position.LerpTo( focus.Position, Time.Delta * 5f );
+		Rotation = Rotation.Slerp( Rotation, focus.Rotation, Time.Delta * 5f );
 
-//		DebugOverlay.Line( Position, Position + Rotation.Forward * 10f, Color.White, 0f );
-//		DebugOverlay.Sphere( Position, 1, Color.White, 0 );
+		//DebugOverlay.Line( Position, Position + Rotation.Forward * 10f, Color.White, 0.5f );
+		//DebugOverlay.Sphere( Position, 1, Color.White, 0 );
 
 		base.Tick();
 	}
--- code/UI/ScoreWidget.cs
+++ code/UI/ScoreWidget.cs
@@ -1,10 +1,19 @@
 namespace TableTennis;
 
 [UseTemplate]
-public partial class ScoreWidget : FocusWorldPanel
+public partial class ScoreWidget : WorldPanel
 {
+	Vector2 Size => new( 1200, 64 );
 	public ScoreWidget()
 	{
-		PanelBounds = new( -64f, -64f, 128f, 128f );
+	}
+
+	public override void Tick()
+	{
+		base.Tick();
+
+		Position = new Vector3( -1.2f, 0f, 33f );
+		Rotation = Rotation.FromYaw( 180f );
+		PanelBounds = new( -Size.x / 2f, -Size.y / 2f, Size.x, Size.y );
 	}
 }
