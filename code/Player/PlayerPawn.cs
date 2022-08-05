73d9186d2458706a615d3d2df8e0bdc3f92f0416
Tony <devultj@gmail.com>
2022-08-05 22:20:29 +0100

    Give players the VR Head, and attach it to their HMD when possible.

--- code/Player/PlayerPawn.cs
+++ code/Player/PlayerPawn.cs
@@ -4,6 +4,8 @@ public partial class PlayerPawn : Entity
 {
 	[Net] public Paddle Paddle { get; set; }
 
+	protected ModelEntity HeadModel { get; set; }
+
 	public override void Spawn()
 	{
 		Paddle = new();
@@ -11,6 +13,13 @@ public partial class PlayerPawn : Entity
 
 		Transmit = TransmitType.Always;
 		Predictable = true;
+	}
+
+	public override void ClientSpawn()
+	{
+		base.ClientSpawn();
+
+		HeadModel = new( "models/vrhead/vrhead.vmdl", this );
 	}
 
 	protected override void OnDestroy()
@@ -22,13 +31,27 @@ public partial class PlayerPawn : Entity
 	{
 		base.Simulate( cl );
 
+		if ( cl.IsUsingVr )
+			EyePosition = Input.VR.Head.Position;
+		else
+			EyePosition = Position + Vector3.Up * 50f;
+	
 		Paddle?.Simulate( cl );
 	}
 
 	public override void FrameSimulate( Client cl )
 	{
 		base.FrameSimulate( cl );
-
 		Paddle?.FrameSimulate( cl );
+	}
+
+	[Event.Frame]
+	protected void UpdateHeadSpot()
+	{
+		if ( HeadModel.IsValid() )
+		{
+			HeadModel.Position = EyePosition;
+			HeadModel.Rotation = EyeRotation;
+		}
 	}
 }
