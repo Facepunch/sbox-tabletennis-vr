fe64c4cf3a8b0f0279ca4e8453f09b8d9efe37dc
Tony <devultj@gmail.com>
2022-08-05 22:45:46 +0100

    Added Team, assign teams on join. Can spawn ball outside of VR mode.

--- code/UI/ScoreWidget.cs
+++ code/UI/ScoreWidget.cs
@@ -12,6 +12,7 @@ public partial class ScoreWidget : WorldPanel
 	{
 		base.Tick();
 
+		// TODO - Decide where this is based on what team you're on
 		Position = new Vector3( -1.2f, 0f, 33f );
 		Rotation = Rotation.FromYaw( 180f );
 		PanelBounds = new( -Size.x / 2f, -Size.y / 2f, Size.x, Size.y );
