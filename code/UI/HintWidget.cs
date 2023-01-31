d4a9610ce99856d8becca207b974347f33b217c0
Tony Ferguson <devultj@gmail.com>
2023-01-31 03:39:11 +0000

    Converted HintWidget to Razor

--- code/UI/HintWidget.cs
+++ code/UI/HintWidget.cs
@@ -1,67 +1,7 @@
-namespace TableTennis;
+namespace TableTennis.UI;
 
-internal class HintWidgetEntry : Panel
+public partial class HintWidget
 {
-	public HintWidgetEntry( string text, string icon = null, float lifetime = 5f, string className = null )
-	{
-		if ( !string.IsNullOrEmpty( icon ) )
-		{
-			if ( icon.StartsWith( "avatar:" ) )
-				Add.Image( icon, "avatar" );
-			else
-				Add.Label( icon, "icon" );
-		}
-
-		if ( !string.IsNullOrEmpty( className ) )
-			SetClass( className, true );
-
-		Add.Label( text, "message" );
-		_ = DeleteAsync( lifetime );
-	}
-
-	protected async Task DeleteAsync( float time )
-	{
-		await GameTask.DelayRealtimeSeconds( time );
-		Delete();
-	}
-}
-
-[UseTemplate]
-public partial class HintWidget : WorldPanel
-{
-	public static HintWidget Current;
-
-	// @ref
-	public Panel Canvas { get; set; }
-	
-	Vector2 Size => new( 1200, 500f );
-	public HintWidget()
-	{
-		Current = this;
-		PanelBounds = new( -Size.x / 2f, -Size.y / 2f, Size.x, Size.y );
-	}
-
-	public void AddEntry( string text, string icon = null, float lifetime = 5f, string className = null )
-	{
-		Canvas.AddChild( new HintWidgetEntry( text, icon, lifetime, className ) );
-	}
-
-	public override void Tick()
-	{
-		base.Tick();
-
-		var game = TableTennisGame.Current;
-		if ( !game.IsValid() )
-			return;
-
-		var myTeam = Game.LocalClient?.GetTeam();
-		if ( myTeam == null )
-			return;
-
-		Position = myTeam.UIAnchor.Position + Vector3.Up * 17f;
-		Rotation = myTeam.UIAnchor.Rotation;
-	}
-
 	[ConCmd.Client( "tt_addmessage", CanBeCalledFromServer = true )]
 	public static void AddMessage( string message, string icon = null, float lifetime = 5f, string className = null )
 	{
