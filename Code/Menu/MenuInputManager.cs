dee333580ecf591fe177d65a514644bd598b21c9
Tony Ferguson <devultj@gmail.com>
2024-05-23 10:09:14 +0100

    Demonstrate world input issue

--- Code/Menu/MenuInputManager.cs
+++ Code/Menu/MenuInputManager.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace Sandbox;
 
 [Title( "Menu Input" ), Icon( "linear_scale" )]
@@ -39,5 +41,23 @@ public sealed class MenuInputManager : Component
 		worldInput.MouseLeftPressed = Input.Down( "Attack1" );
 		worldInput.MouseRightPressed = Input.Down( "Attack2" );
 		worldInput.MouseWheel = Input.MouseWheel;
+
+		var rootPanel = worldInput.Hovered?.FindRootPanel();
+
+		var dist = -1f;
+
+		if ( rootPanel is not null )
+		{
+			// Fetch how far away the panel is in world-space so we can draw a line to it.
+			rootPanel.RayToLocalPosition( worldInput.Ray, out _, out dist );
+		}
+
+		if ( dist == -1f ) return;
+
+		var tr = Scene.Trace.Ray( Scene.Camera.Transform.Position, Scene.Camera.Transform.Position + Scene.Camera.Transform.Rotation.Forward * dist )
+			.IgnoreGameObject( GameObject )
+			.Run();
+
+		Gizmo.Draw.LineSphere( tr.EndPosition, 1 );
 	}
 }
